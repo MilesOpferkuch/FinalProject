66c815b9a8f3f0c46ecd1b616b91704925628fa1
MilesOpferkuch <mbopferk@ucsc.edu>
2023-05-18 13:04:35 -0700

    Added date pickers

--- node/server.js
+++ node/server.js
@@ -4,29 +4,22 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello World! Welcome to Node.js')
-})
-
 app.get("/tides/:station/:begin_date/:end_date/:tz", (req, res) => {
-    console.log("wtf");
     const station = req.params.station;
     const begin_date = req.params.begin_date;
     const end_date = req.params.end_date;
     const tz = req.params.tz;
-    const url = `https://tidesandcurrents.noaa.gov/api/datagetter?product=predictions&begin_date=${begin_date}&end_date=${end_date}&datum=MLLW&station=${station}&time_zone=${tz}&units=english&interval=1&format=json`
+    const url = `https://tidesandcurrents.noaa.gov/api/datagetter?product=predictions&begin_date=${begin_date}&end_date=${end_date}&datum=MLLW&station=${station}&time_zone=${tz}&units=english&interval=10&format=json`
 
     request(url, (error, response, body) => {
         if (error) {
             return res.status(500).send("Error retrieving tide data.");
         }
-        body = JSON.parse(body);
-        console.log(body);
-        res.send(body.main);
+        res.send(body);
     })
 });
 
-app.get("metadata/:station", (req, res) => {
+app.get("/metadata/:station", (req, res) => {
     const station = req.params.station;
     const url = `https://api.tidesandcurrents.noaa.gov/mdapi/prod/webapi/stations/${station}.json?units=english`
 
@@ -34,8 +27,7 @@ app.get("metadata/:station", (req, res) => {
         if (error) {
             return res.status(500).send("Error retrieving station metadata.");
         }
-        body = JSON.parse(body);
-        console.log(body);
+        res.send(body);
     })
 });
 
@@ -51,10 +43,9 @@ app.get("suntimes/:lat/:lon/:tz/:date", (req, res) => {
             return res.status(500).send("Error retrieving sunrise/sunset data.");
         }
         body = JSON.parse(body);
-        console.log(body);
+        res.send(body);
     })
-
-})
+});
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!');
--- script.js
+++ script.js
@@ -1,17 +1,39 @@
-const station = 9410230;
-const lat = 0;
-const lon = 0;
-const begin_date = "20230517";
-const end_date = "20230518";
-const tz = "lst_ldt";
+let station = 9410230;
+let lat = 0;
+let lon = 0;
+let begin_date = "20230517";
+let end_date = "20230518";
+let tz = "lst_ldt";
+let date = new Date();
+let year = date.getFullYear().toString();
+let month = date.getMonth() + 1;
+let day = date.getDate();
+
+// Zero-pad date
+if (month < 10) {
+    month = month.toString().padStart(2, "0");
+}
+if (day < 10) {
+    day = day.toString().padStart(2, "0");
+}
+let dateString = `${year}-${month}-${day}`
 
 window.onload = function() {
-    console.log("here")
+
+    const beginDateSelector = document.getElementById("begin_date");
+    beginDateSelector.value = dateString;
+    beginDateSelector.min = dateString;
+    const endDateSelector = document.getElementById("end_date");
+    endDateSelector.value = dateString;
+    endDateSelector.min = dateString;
+
+    console.log(dateString);
     const xhr = new XMLHttpRequest();
-    xhr.open("GET", `http://localhost:3000/tides/${station}/${begin_date}/${end_date}/${tz}`);
+    //xhr.open("GET", `http://localhost:3000/tides/${station}/${begin_date}/${end_date}/${tz}`);
+    xhr.open("GET", `http://localhost:3000/metadata/${station}`);
     xhr.send();
     xhr.onload = function() {
         const body = JSON.parse(xhr.responseText);
-        console.log(body);
+        console.log(body.stations[0].name);
     }
-}
+};
